f435d2a85787663847bb8a14b37752fd13a6e1e7
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-05-30 16:59:28 +0200

    conditions for divisible by 5, 3 or none

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ for (var i = 1; i <= 100; i++) {
 
     // if the current number is divisible by both 3 and 5 using the condition.
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz"); 
-    }
-}
+        console.log("FizzBuzz");
+
+        // check if the number is divisible by 3 using the condition.
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+
+        // check if the number is divisible by 5 using the condition.
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+
+        // If none of the above conditions are met, print the current number.
+    } else {
+        console.log(i);
+    }    
+}   
