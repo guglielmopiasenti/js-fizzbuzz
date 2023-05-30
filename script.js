70589e5c02ceee829e9b56fe182fb70e66c255a6
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-05-30 16:53:11 +0200

    Setting up loop and FizzBuzz

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("JS OK");
 
-// Set up a `for` loop to iterate from 1 to 100.
+// I set up a `for` loop to iterate from 1 to 100.
 for (var i = 1; i <= 100; i++) {
+
+    // if the current number is divisible by both 3 and 5 using the condition.
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz"); 
+    }
 }
